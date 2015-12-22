f1ee2dd3ad64b79ae6fc510792d0f0cbdfb903f4
EthanK28 <kes0228@gmail.com>
2015-12-22 20:04:11 +0900

    Likes field added to Category model

--- rango/models.py
+++ rango/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 class Page(models.Model):
@@ -14,5 +14,5 @@ class Page(models.Model):
     url = models.URLField()
     views = models.IntegerField(default=0)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
