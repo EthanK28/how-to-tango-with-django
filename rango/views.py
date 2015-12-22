8b6dc262266c87e705f894cf6ea2f607b94d1e42
EthanK28 <kes0228@gmail.com>
2015-12-22 19:15:21 +0900

    Template Finished

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Rango says hey there world!");
+    context_dict = {'boldmessage' : " Iam bold font from the context "}
+
+    return render(request, 'rango/index.html', context_dict)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -11,7 +11,13 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 
+
+TEMPLATE_DIRS = (
+
+    TEMPLATE_PATH,
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -25,6 +31,8 @@ DEBUG = True
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
+
+
 
 
 # Application definition
@@ -81,4 +89,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+
+STATIC_URL = '/static/' # You may find this is already defined as such.
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
